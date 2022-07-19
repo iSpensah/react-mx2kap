9e7396ca2b9db0d29d6ec7b8d9487130b405055d
iSpensah <crazymeet1998@gmail.com>
2022-07-19 12:36:54 +0800

    1 file added

---
+++ src/Food.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import TableRow from './TableRow';
+
+class Food extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      foods: [
+        { FoodId: 123, FoodName: 'Hotdog', FoodClass: 'fastfood' },
+        { FoodId: 124, FoodName: 'Bacon', FoodClass: 'Pork' },
+        { FoodId: 125, FoodName: 'Apple', FoodClass: 'Fruits' },
+        { FoodId: 126, FoodName: 'CauliFlower', FoodClass: 'Vegetable' },
+      ],
+    };
+  }
+
+  reder() {
+    return (
+      <table border="1">
+        <tr>
+          <th>FoodID</th>
+          <th>FoodName</th>
+          <th>FoodClass</th>
+        </tr>
+        <tbody>
+          {this.state.foods.map((f) => (
+            <TableRow
+              FoodId={f.FoodId}
+              FoodName={f.FoodName}
+              FoodClass={f.FoodClass}
+            />
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+} //class bracket
+export default Food;
