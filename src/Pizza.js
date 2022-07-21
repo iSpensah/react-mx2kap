5bdef2cf85fd8da07a9cf1bcbec2c4aa02bfff83
iSpensah <crazymeet1998@gmail.com>
2022-07-21 14:50:56 +0800

    1 file modified

--- src/Pizza.js
+++ src/Pizza.js
@@ -1,10 +1,11 @@
-//import './person.css';     //THIS IS THE WAY of adding css to your component
+import './person.css'; //THIS IS THE WAY of adding css to your component
 import React, { Component } from 'react';
 import Button from './Button';
 class Pizza extends React.Component {
   constructor() {
     super();
     this.state = { slices: 8, Food: 'Pizza' };
+    this.state;
     this.eatSlice = this.eatSlice.bind(this);
     this.buySlice = this.buySlice.bind(this);
   }
@@ -23,12 +24,19 @@ class Pizza extends React.Component {
     });
   }
 
+  changeState() {
+    this.setState({ Food: 'HamBurger' });
+  }
+
   render() {
     return (
       <div>
         <Button action={this.eatSlice} label="Eat a slice" />
-        <p> Pizza number of slices is {this.state.slices}</p>
+        <p>
+          {this.state.Food} number of slices is {this.state.slices}
+        </p>
         <Button action={this.buySlice} label="Buy a slice" />
+        <Button onchange={() => this.state.changeState} />
       </div>
     );
   }
